d9c7576814c5b868b799744670ca539db549c686
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-09-22 13:46:17 -0700

    Reorder functions in ConnectionServer

--- lib/ticket_to_ride/connection_server.ex
+++ lib/ticket_to_ride/connection_server.ex
@@ -5,26 +5,6 @@ defmodule TicketToRide.ConnectionServer do
 
   @type t :: %City{}
 
-  def handle_call(:connected?, _from, [origin, dest, cities_already_checked]) do
-    {:reply, connected?(origin, dest, cities_already_checked), []}
-  end
-
-  def handle_cast(:connected?, [_origin, _dest, _cities_already_checked]) do
-    {:noreply, :not_implemented}
-  end
-
-  def start_link(default) do
-    GenServer.start_link(__MODULE__, default)
-  end
-
-  ### NOT IMPLEMENTED YET
-
-  def code_change(_old_vsn, _state, _extra), do: {:error, :not_implemented}
-
-  def handle_info(_msg, state), do: {:noreply, state}
-
-  def terminate(_reason, _state), do: :ok
-
   @spec connected?(t, t, [t]) :: boolean
   def connected?(%City{} = city,   %City{} = city, _), do: true
   def connected?(%City{} = origin, %City{} = dest, cities_already_checked) do
@@ -33,9 +13,27 @@ defmodule TicketToRide.ConnectionServer do
     |> Enum.any?(&spawn_connected?(&1, dest, cities_already_checked))
   end
 
+  def handle_call(:connected?, _from, [origin, dest, cities_already_checked]) do
+    {:reply, connected?(origin, dest, cities_already_checked), []}
+  end
+
+  def handle_cast(:connected?, [_origin, _dest, _cities_already_checked]) do
+    {:noreply, :not_implemented}
+  end
+
+  def handle_info(_msg, state), do: {:noreply, state}
+
+  def start_link(default), do: GenServer.start_link(__MODULE__, default)
+
+  def terminate(_reason, _state), do: :ok
+
+  ### NOT IMPLEMENTED YET
+  def code_change(_old_vsn, _state, _extra), do: {:error, :not_implemented}
+
   ### PRIVATE FUNCTIONS
 
   defp spawn_connected?(origin, dest, cities_already_checked) do
+    # OPTIMIZE: Any way to remove semi-duplication with City.connected?/2 ?
     gs_args    = [origin, dest, [origin | cities_already_checked]]
     {:ok, pid} = GenServer.start_link(__MODULE__, gs_args)
     GenServer.call(pid, :connected?)
